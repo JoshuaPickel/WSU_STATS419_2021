62f0c4965fe44b44bb78008a840c50ec5a8414a7
joshua pickel <josh.m.pickel@verizon.net>
2021-02-02 22:17:19 -0800

    Line color

--- functions/functions-intro/r-intro.R
+++ functions/functions-intro/r-intro.R
@@ -23,8 +23,8 @@ handShake = function(n=1, plotMe=FALSE)
       c = rep("red",45)
       allPeople = combn(people,2)
       graph = graph( edges = allPeople, directed = T)
-      plot(graph,edge.arrow.mode = 3, edge.arrow.size = .25, edge.color = c, vertex.size = 25,vertex.color = "grey",
-           vertex.frame.color = "black", edge.color = rainbow(number_of_handShakes), layout = layout_in_circle )
+      plot(graph,edge.arrow.mode = 3, edge.arrow.size = .25, vertex.size = 25,vertex.color = "grey",
+           vertex.frame.color = "black", edge.color = c, layout = layout_in_circle )
     }
     sprintf("The number of handshakes between %i people is %i", n, number_of_handShakes)
   }
